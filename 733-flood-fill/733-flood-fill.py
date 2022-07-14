78f26b78b6abd505676bc1aaa843f0403de4b59f
sruthi2498 <vsruthi98@gmail.com>
2022-07-14 08:52:10 -0700

    Time: 153 ms (12.95%), Space: 14 MB (65.83%) - LeetHub

--- 733-flood-fill/733-flood-fill.py
+++ 733-flood-fill/733-flood-fill.py
@@ -2,18 +2,15 @@ class Solution:
     def floodFill(self, image: List[List[int]], sr: int, sc: int, color: int) -> List[List[int]]:
         n = len(image)
         m = len(image[0])
-        
         stack = [(sr,sc)]
         startColor = image[sr][sc]
         visited = set()
-        toBeColored = []
         while len(stack) > 0 :
             # print(stack)
             i,j = stack.pop(0)
             if image[i][j]==startColor:
                 # print(i,j)
                 image[i][j]=color
-                toBeColored.append([i,j])
             visited.add((i,j))
             if i-1>=0 and image[i-1][j]==startColor and (i-1,j) not in visited:
                 stack.append([i-1,j])
@@ -23,7 +20,5 @@ class Solution:
                 stack.append([i,j-1])
             if j+1<m and image[i][j+1]==startColor and (i,j+1) not in visited:
                 stack.append([i,j+1])
-        # for i,j in toBeColored:
-        #     image[i][j] = color
         return image
                 

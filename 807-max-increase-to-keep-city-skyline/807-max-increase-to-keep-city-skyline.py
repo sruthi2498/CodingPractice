dd1054368b6942d25cf2359a64a63c0967d9e826
sruthi2498 <vsruthi98@gmail.com>
2022-07-29 20:06:17 -0700

    Time: 297 ms (7.60%), Space: 32.2 MB (5.31%) - LeetHub

--- 807-max-increase-to-keep-city-skyline/807-max-increase-to-keep-city-skyline.py
+++ 807-max-increase-to-keep-city-skyline/807-max-increase-to-keep-city-skyline.py
@@ -4,7 +4,7 @@ class Solution:
         grid = np.array(grid)
         rowMax = [max(row) for row in grid]
         colMax = [max(grid[:,i]) for i in range(len(grid))]
-        print(rowMax,colMax)
+        # print(rowMax,colMax)
         total = 0
         for i in range(len(grid)):
             for j in range(len(grid)):

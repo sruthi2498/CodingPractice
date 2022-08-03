7dcc45c975ed5f7db619bedd099fdc5a87131a7c
sruthi2498 <vsruthi98@gmail.com>
2022-08-03 12:05:32 -0700

    Time: 51 ms (42.16%), Space: 13.9 MB (31.70%) - LeetHub

--- 17-letter-combinations-of-a-phone-number/17-letter-combinations-of-a-phone-number.py
+++ 17-letter-combinations-of-a-phone-number/17-letter-combinations-of-a-phone-number.py
@@ -1,6 +1,7 @@
 class Solution:
     def __init__(self):
         self.map = {"0":[""],"1":[""],"2":["a","b","c"],"3":["d","e","f"], "4":["g","h","i"],"5":["j","k","l"],"6":["m","n","o"],"7":["p","q","r","s"],"8":["t","u","v"],"9":["w","x","y","z"]}
+        self.cache = {}
      
     def letterCombinations(self, digits: str) -> List[str]:
         n = len(digits)
@@ -10,12 +11,7 @@ class Solution:
             return self.map[digits[0]]
         
         res = []
-            
-        if n==2:
-            for a in self.map[digits[0]]:
-                for b in self.map[digits[1]]:
-                    res.append(a+b)
-            return res
+        
         
         for a in self.map[digits[0]]:
             for b in self.letterCombinations(digits[1:]):

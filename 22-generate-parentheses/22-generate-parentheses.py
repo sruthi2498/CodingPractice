3a8be51c7aabf0a855a39e48cd02dd2dbeb24ad5
sruthi2498 <vsruthi98@gmail.com>
2022-08-02 18:49:43 -0700

    Time: 65 ms (29.00%), Space: 14.4 MB (11.68%) - LeetHub

--- 22-generate-parentheses/22-generate-parentheses.py
+++ 22-generate-parentheses/22-generate-parentheses.py
@@ -4,19 +4,18 @@ class Solution:
     def generateParenthesis(self, n: int) -> List[str]:
         if n in self.pars:
             return self.pars[n]
-        
         result = set()
-        prev = self.generateParenthesis(n-1)
-        for p in prev:
-            result.add("("+p+")")
-        
         for i in range(1,n):
             # print(i,n-i)
-            first = self.generateParenthesis(i)
-            second = self.generateParenthesis(n-i)
+            first = self.generateParenthesis(i) if i not in self.pars else self.pars[i]
+            second = self.generateParenthesis(n-i) if n-i not in self.pars else self.pars[n-i]
             for f in first:
                 for s in second:
                     result.add(f+s)
+        prev = self.generateParenthesis(n-1) if n-1 not in self.pars else self.pars[n-1]
+        for p in prev:
+            result.add("("+p+")")
+         
         self.pars[n] = list(result)
         return self.pars[n]
         

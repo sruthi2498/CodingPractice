a0787e8fdda584db6dd8eafc21093e47938485de
sruthi2498 <vsruthi98@gmail.com>
2022-08-08 14:13:36 -0700

    Time: 939 ms (60.48%), Space: 37.8 MB (5.38%) - LeetHub

---
+++ 134-gas-station/134-gas-station.py
@@ -0,0 +1,17 @@
+import numpy as np
+
+class Solution:
+    def canCompleteCircuit(self, gas: List[int], cost: List[int]) -> int:
+        if (sum(gas) - sum(cost) < 0):
+            return -1
+        
+        gas_tank, start_index = 0, 0
+        
+        for i in range(len(gas)):
+            gas_tank += gas[i] - cost[i]
+            
+            if gas_tank < 0:
+                start_index = i+1
+                gas_tank = 0
+            
+        return start_index

819ea95903e2188db21897ceff9e4d7e46b59912
sruthi2498 <vsruthi98@gmail.com>
2022-09-03 13:08:55 -0700

    Time: 4195 ms (34.37%), Space: 19.9 MB (39.35%) - LeetHub

---
+++ 2304-minimum-path-cost-in-a-grid/2304-minimum-path-cost-in-a-grid.py
@@ -0,0 +1,26 @@
+class Solution:
+    def minPathCost(self, grid: List[List[int]], moveCost: List[List[int]]) -> int:
+        n = len(grid)
+        m = len(grid[0])
+        opt = []
+        for _ in range(n):
+            opt.append([math.inf]*m)
+        for j in range(m):
+            opt[0][j] = grid[0][j]
+            
+#         for row in opt:
+#             print(row)
+            
+        for i in range(1,n):
+            for j in range(m):
+                for k in range(m):
+                    opt[i][j] = min(opt[i][j], 
+                                   opt[i-1][k] + moveCost[grid[i-1][k]][j]
+                                   + grid[i][j])
+        for row in opt:
+            print(row)
+        result = math.inf    
+        for j in range(m):
+            result = min(result, opt[-1][j])
+            
+        return result

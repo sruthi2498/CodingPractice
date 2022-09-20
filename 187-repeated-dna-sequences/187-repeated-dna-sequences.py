ae8845e52571fe5c331f1c6006b8755048849d1b
sruthi2498 <vsruthi98@gmail.com>
2022-09-20 11:01:32 -0700

    Time: 189 ms (8.50%), Space: 27.7 MB (37.77%) - LeetHub

--- 187-repeated-dna-sequences/187-repeated-dna-sequences.py
+++ 187-repeated-dna-sequences/187-repeated-dna-sequences.py
@@ -2,8 +2,20 @@ from collections import Counter
 class Solution:
     def findRepeatedDnaSequences(self, s: str) -> List[str]:
         n = len(s)
-        substrings = [s[i:i+10] for i in range(n-9)]
-        counter = Counter(substrings)
+        counter = {}
+        i = 0
+        while i+9<n:
+            j = i+10
+            substring = s[i:j]
+            if substring not in counter:
+                counter[substring] = 0
+            counter[substring]+=1
+            i+=1
+            
+        
+        
+        # substrings = [s[i:i+10] for i in range(n-9)]
+        # counter = Counter(substrings)
         result = []
         for k,v in counter.items():
             if v>1:

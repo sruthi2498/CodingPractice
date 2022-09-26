d6e8e9eade1e2f0a911f267028cba57443a0d4d4
sruthi2498 <vsruthi98@gmail.com>
2022-09-26 10:26:17 -0700

    Time: 1662 ms (20.27%), Space: 32.9 MB (81.53%) - LeetHub

---
+++ 2332-the-latest-time-to-catch-a-bus/2332-the-latest-time-to-catch-a-bus.py
@@ -0,0 +1,37 @@
+class Solution:
+    def latestTimeCatchTheBus(self, buses: List[int], passengers: List[int], capacity: int) -> int:
+        buses.sort()
+        passengers.sort()
+        
+        n = len(buses)
+        m = len(passengers)
+        print(buses)
+        print(passengers)
+        
+        i = 0
+        j = 0
+        while i<n:
+            currentCap = 0
+            while j<m and passengers[j]<=buses[i] and currentCap<capacity:
+                currentCap+=1
+                j+=1
+            # print("bus ",i,buses[i])
+            # print("\tcurrentCap ",currentCap)
+            # print("\ttill passenger ",j-1)
+            
+            i+=1
+        if i<n:
+            return buses[-1]
+        
+        k = j-1
+        if currentCap < capacity:
+            time = buses[i-1]
+        else:
+            time = passengers[k]
+        
+        # print("starting at k=",k,"time = ",time)
+        while k>=0 and passengers[k]==time:
+            k-=1
+            time-=1
+        return time
+        

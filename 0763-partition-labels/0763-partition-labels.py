7f8996d702217ffc4f6965ef53b344642ee1177d
sruthi2498 <vsruthi98@gmail.com>
2022-11-05 09:38:47 -0700

    Time: 44 ms (90.87%), Space: 13.9 MB (68.38%) - LeetHub

---
+++ 0763-partition-labels/0763-partition-labels.py
@@ -0,0 +1,28 @@
+class Solution:
+    def partitionLabels(self, s: str) -> List[int]:
+        i = 0
+        n = len(s)
+        char_map = {}
+        for i in range(n):
+            if s[i] not in char_map:
+                char_map[s[i]]=[i,i]
+            else:
+                char_map[s[i]][1]=i
+        # print(char_map)
+        
+        
+        i=0
+        result = []
+        while i<n:
+            start = char_map[s[i]][0]
+            end = char_map[s[i]][1]
+            j = start+1
+            while j<end:
+                if(char_map[s[j]][1] >end):
+                    end = char_map[s[j]][1]
+                j+=1
+            print(s[start:end+1],len(s[start:end+1]))
+            result.append(len(s[start:end+1]))
+            i = end+1
+        return result
+        
